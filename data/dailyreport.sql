e8aa005b71b08c88041f3f5d9c5d1c92e0cb3b62
giaduc <lgduc1913@gmail.com>
2018-03-17 10:07:39 +0700

    update database

--- data/dailyreport.sql
+++ data/dailyreport.sql
@@ -17,8 +17,8 @@ GO
 -- CREATE TABLE RIGHTS
 CREATE TABLE rights (
     id int IDENTITY(1,1) PRIMARY KEY NOT NULL,
-    name nvarchar(150),
-    lv int,
+    name nvarchar(50),
+    level int,
     created_at datetime,
     updated_at datetime
 );
@@ -27,8 +27,8 @@ GO
 -- CREATE TABLE PROJECTS
 CREATE TABLE projects (
 	id int IDENTITY(1,1) PRIMARY KEY NOT NULL,
-    project_code varchar(18) NOT NULL,
-    name nvarchar(150),
+    project_code varchar(4) NOT NULL,
+    name nvarchar(50),
     start_at date,
     finish_at date,
     created_at datetime,
@@ -39,7 +39,7 @@ GO
 -- CREATE TABLE USERS
 CREATE TABLE users (
 	id int IDENTITY(1,1) PRIMARY KEY NOT NULL,
-    employee_code varchar(18) NOT NULL,
+    employee_code varchar(12) NOT NULL,
     password varchar(18),
     rights int,
     name nvarchar(150),
@@ -53,8 +53,8 @@ GO
 -- CREATE TABLE REPORTS
 CREATE TABLE tasks (
     id int IDENTITY(1,1) PRIMARY KEY NOT NULL,
-    task_code varchar(18),
-    name nvarchar(150),
+    task_code varchar(4),
+    name nvarchar(50),
     created_at datetime,
     updated_at datetime
 );
@@ -63,17 +63,15 @@ GO
 -- CREATE TABLE REPORTS
 CREATE TABLE reports (
     id int IDENTITY(1,1) PRIMARY KEY NOT NULL,
-    worked_at time(7),
-    worked_to time(7),
-    note text,
+    time_worked time(7),
+    note nvarchar(500),
 	project_id int,
 	task_id int,
 	user_id int,
-    created_at datetime,
-	updated_at datetime,
-	approver int,
-	approval_status int,
+    approver int,
 	approved_at datetime,
+	created_at datetime,
+	updated_at datetime,
 	CONSTRAINT FK_project_report FOREIGN KEY (project_id)
     REFERENCES projects(id),
     CONSTRAINT FK_task_report FOREIGN KEY (task_id)
@@ -87,17 +85,17 @@ GO
 
 
 -- INSERT INTO RIGHTS
-INSERT INTO rights(name, lv)
-VALUES ('director', 1); 
+INSERT INTO rights(name, level)
+VALUES ('director', 3); 
 
-INSERT INTO rights(name, lv)
+INSERT INTO rights(name, level)
 VALUES ('manager', 2);
 
-INSERT INTO rights(name, lv)
-VALUES ('leader', 3);
+INSERT INTO rights(name, level)
+VALUES ('leader', 1);
 
 -- INSERT INTO USERS
 INSERT INTO users(employee_code, password, rights, name)
 VALUES ('hanh', '1111', 1, 'Nguyen Hanh'); 
-INSERT INTO users(employee_code, password, rights, name)
-VALUES ('giaduc', '1111', 1, 'Gia duc'); 
+INSERT INTO users(employee_code, password, name)
+VALUES ('giaduc', '1111', 'Gia duc'); 
