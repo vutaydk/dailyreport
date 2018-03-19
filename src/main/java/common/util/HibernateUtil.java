76e88eee847739d18b8bf5c50926497b6470de8c
Pham Ba Vu <phambavu@gmail.com>
2018-03-19 08:48:28 +0700

    change hibernate entity config

--- src/main/java/common/util/HibernateUtil.java
+++ src/main/java/common/util/HibernateUtil.java
@@ -1,6 +1,5 @@
 package common.util;
 
-
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -9,14 +8,9 @@ public class HibernateUtil {
 	private static SessionFactory sessionFactory;
 
 	public static SessionFactory getSessionFactory() {
-		try {
-			if (sessionFactory == null) {
-				sessionFactory = new Configuration().configure().buildSessionFactory();
-			}
-		} catch (Exception e) {
-			System.err.println(e);
-		} finally {
-			System.out.println("SessionFatory: " + (sessionFactory != null));
+		if (sessionFactory == null) {
+			Configuration conf = new Configuration().configure();
+			sessionFactory = conf.buildSessionFactory();
 		}
 		return sessionFactory;
 	}
