26308f0a653dc7e50268bb0eedcba8113e02e927
Hanh <hanh.cho.do@gmail.com>
2018-03-23 14:55:05 +0700

    remove edit + add sevlet, change packages class sevlet

--- src/main/java/controller/ReportSevlet.java
+++ src/main/java/controller/ReportSevlet.java
@@ -1,4 +1,4 @@
-package controller.report;
+package controller;
 
 import java.io.IOException;
 import java.sql.Date;
--- src/main/java/controller/UserSevlet.java
+++ src/main/java/controller/UserSevlet.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Optional;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,17 +10,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import model.entity.User;
-import model.repo.UserRepo;
+import common.util.DataValidation;
+import common.util.Format;
+import model.entity.Project;
+import model.repo.ProjectRepo;
 
-@WebServlet("/admin/user")
+@WebServlet("/user")
 public class UserSevlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	private UserRepo userDao;
+
+	protected ProjectRepo projectDao;
+	private HashMap<String, String> hashMap;
 
 	public UserSevlet() {
-		userDao = new UserRepo();
+		projectDao = new ProjectRepo();
+		hashMap = new HashMap<>();
 	}
 
 	/**
@@ -26,7 +33,8 @@ public class UserSevlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		request.getRequestDispatcher("/views/user.jsp").forward(request, response);
+		request.setAttribute("listProject", projectDao.getAll());
+		request.getRequestDispatcher("/WEB-INF/jsps/user/index.jsp").forward(request, response);
 	}
 
 	/**
@@ -34,21 +42,84 @@ public class UserSevlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		User obUser = new User();
-		obUser.setEmployeeCode("");
-		obUser.setName("");
-		obUser.setPassword("");
-		obUser.setRights(null);
-		// INSERT
-		if (userDao.insert(obUser)) {
-			// redirect
+		doGet(request, response);
+	}
+
+	/**
+	 * Get errors
+	 * 
+	 * @return HashMap
+	 */
+	protected HashMap<String, String> getError() {
+		return hashMap;
+	}
+
+	/**
+	 * Validate form
+	 * 
+	 * @param request
+	 * @param response
+	 * @param project
+	 * @return boolean
+	 */
+	protected boolean validate(HttpServletRequest request, HttpServletResponse response, Project project) {
+
+		boolean bool = true;
+		hashMap.clear();
+
+		// get request param
+		Optional<String> txt_projectCode = Optional.ofNullable(request.getParameter("txt_projectCode"));
+		if (!txt_projectCode.isPresent() || txt_projectCode.get().isEmpty()) {
+			hashMap.put("txt_projectCode", "Please enter project code.");
+			bool = false;
+		} else {
+			project.setProjectCode(txt_projectCode.get());
 		}
 
-		// UPDATE
-		if (userDao.update(obUser)) {
-			// redirect
+		Optional<String> txt_name = Optional.ofNullable(request.getParameter("txt_name"));
+		if (!txt_name.isPresent() || txt_name.get().isEmpty()) {
+			hashMap.put("txt_name", "Please enter name.");
+			bool = false;
+		} else {
+
+			if (txt_name.get().length() < 6) {
+				hashMap.put("txt_name", "Name length is too short (requires 6 characters).");
+				bool = false;
+			} else
+				project.setName(txt_name.get());
 		}
-		doGet(request, response);
+
+		Optional<String> txt_startAt = Optional.ofNullable(request.getParameter("txt_startAt"));
+		if (!txt_startAt.isPresent() || txt_startAt.get().isEmpty()) {
+			hashMap.put("txt_startAt", "Please enter startAt.");
+			bool = false;
+		} else {
+
+			if (!DataValidation.isValidDate(txt_startAt.get())) {
+				hashMap.put("txt_startAt", "Invalid startAt.");
+				bool = false;
+			} else {
+
+				project.setStartAt(Format.toDate(txt_startAt.get()));
+			}
+		}
+
+		Optional<String> txt_finishAt = Optional.ofNullable(request.getParameter("txt_finishAt"));
+		if (!txt_finishAt.isPresent() || txt_finishAt.get().isEmpty()) {
+			hashMap.put("txt_finishAt", "Please enter finishAt.");
+			bool = false;
+		} else {
+
+			if (!DataValidation.isValidDate(txt_finishAt.get())) {
+				hashMap.put("txt_finishAt", "Invalid startAt.");
+				bool = false;
+			} else {
+
+				project.setFinishAt(Format.toDate(txt_finishAt.get()));
+			}
+		}
+
+		return bool;
 	}
 
 }
