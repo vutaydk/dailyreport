a58d6c94eee1efcd90c38deb3ad8324bf733890f
Mr.Hanh <hanh.cho.do@gmail.com>
2018-04-02 12:03:11 +0700

    add debug with log4j

--- src/main/java/controller/dispatcher/RightsSevlet.java
+++ src/main/java/controller/dispatcher/RightsSevlet.java
@@ -8,7 +8,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import lombok.extern.log4j.Log4j;
+
 @WebServlet("/rights")
+@Log4j
 public class RightsSevlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
@@ -18,6 +21,7 @@ public class RightsSevlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		log.debug("doGet");
 		request.getRequestDispatcher("/WEB-INF/jsps/rights/index.jsp").forward(request, response);
 	}
 
@@ -26,6 +30,7 @@ public class RightsSevlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		log.debug("doPost");
 		doGet(request, response);
 	}
 
