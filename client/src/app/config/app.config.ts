8f50a1ac0371f4a857006c3738629d4f2b86af0e
giaduc <lgduc1913@gmail.com>
2018-04-23 09:35:16 +0700

    Revert "Dev client"

--- client/src/app/config/app.config.ts
+++
@@ -1,5 +0,0 @@
-import { IAppConfig } from "./i.app.config";
-
-export const AppConfig: IAppConfig = {
-    urlAPI: 'http://localhost:4848/api'
-};
--- client/src/app/shared/layout/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './nav/nav.component';
-export * from './sidebar-right/sidebar-right.component';
-export * from './sidebar/sidebar.component';
